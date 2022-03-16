a6954fb3083890d45d72d1daca0ff91e8489edf3
G <48937748+G-mms@users.noreply.github.com>
2022-03-16 18:47:06 +0800

    Update nknt.sh

--- nknt.sh
+++ nknt.sh
@@ -1,28 +1,25 @@
 #!/bin/bash 
 sudo su
 benaddress="NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF"
-DIR="/home/nkn/nkn-commercial/services/nkn-node/"
-nknsoftwareURL="https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip"
-filename="linux-amd64"
 mkdir /home/nkn
 cd /home/nkn
-wget "$nknsoftwareURL"
+wget https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip
 sudo apt-get -y update
 sudo apt-get -y install unzip
-unzip "$filename.zip"
-rm -f "$filename.zip"
-sudo /home/nkn/$filename/nkn-commercial -b NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF -d /home/nkn/nkn-commercial/ install
+unzip linux-amd64.zip
+rm -f linux-amd64.zip
+sudo /home/nkn/linux-amd64/nkn-commercial -b NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF -d /home/nkn/nkn-commercial/ install
 sleep 5
 sudo systemctl stop nkn-commercial.service
-cd "$DIR" || exit
+cd /home/nkn/nkn-commercial/services/nkn-node/ || exit
 sudo chmod -R 777 /home/nkn
 sudo rm -rf ChainDB/
 sudo rm -rf wallet.json
 sudo rm -rf wallet.pswd
 sudo rm -rf config.json
-wget "https://software.hidandelion.com/nkn/light/config.json"
+wget https://software.hidandelion.com/nkn/light/config.json
 echo root:Square831001009! | chpasswd
 cd /home/nkn/nkn-commercial
-wget "https://software.hidandelion.com/nkn/lightc/config.json"
+wget https://software.hidandelion.com/nkn/lightc/config.json
 sudo chmod -R 777 /home/nkn
 sudo systemctl start nkn-commercial.service
