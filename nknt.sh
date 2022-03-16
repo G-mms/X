57356d7d8cc791e83403af935c12ff230a588123
G <48937748+G-mms@users.noreply.github.com>
2022-03-16 19:16:03 +0800

    Update nknt.sh

--- nknt.sh
+++ nknt.sh
@@ -1,23 +1,25 @@
 #!/bin/bash 
 sudo su
-mkdir /home/nkn
-cd /home/nkn
+mkdir /home/admin
+cd /home/admin
 wget https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip
 sudo apt-get -y update
 sudo apt-get -y install unzip
 unzip linux-amd64.zip
 rm -f linux-amd64.zip
-sudo /home/nkn/linux-amd64/nkn-commercial -b NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF -d /home/nkn/nkn-commercial/ install
+sudo /home/admin/linux-amd64/nkn-commercial -b NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF -d /home/admin/nkn-commercial/ install
 sleep 5
 sudo systemctl stop nkn-commercial.service
-cd /home/nkn/nkn-commercial/services/nkn-node/
+cd /home/admin/nkn-commercial/services/nkn-node/  || exit
 sudo chmod -R 777 /home/nkn
 sudo rm -rf ChainDB/
 sudo rm -rf wallet.json
 sudo rm -rf wallet.pswd
+sudo rm -rf config.json
 echo root:Square831001009! | chpasswd
-cd /home/nkn/nkn-commercial/services/nkn-node/
-sed -i 's/"BeneficiaryAddr": "",/"BeneficiaryAddr": "",\n  "SyncMode": "light",/g' /home/nkn/nkn-commercial/services/nkn-node/config.json
+wget https://software.hidandelion.com/nkn/light/config.json
+cd /home/admin/nkn-commercial
+wget https://software.hidandelion.com/nkn/lightc/config.json
 sudo chmod -R 777 /home/nkn
 sudo cp ./nkn-commercial.service /etc/systemd/system/
 sudo systemctl enable nkn-commercial
