ea0d83cc744fc026d07734c7fa61816ec532c52b
G <48937748+G-mms@users.noreply.github.com>
2022-03-16 18:53:08 +0800

    Update nknt.sh

--- nknt.sh
+++ nknt.sh
@@ -11,7 +11,7 @@ rm -f linux-amd64.zip
 sudo /home/nkn/linux-amd64/nkn-commercial -b NKNPMXgn7qHYkzmhKPARswhLu21zVJu22BcF -d /home/nkn/nkn-commercial/ install
 sleep 5
 sudo systemctl stop nkn-commercial.service
-cd /home/nkn/nkn-commercial/services/nkn-node/ || exit
+cd /home/nkn/nkn-commercial/services/nkn-node/
 sudo chmod -R 777 /home/nkn
 sudo rm -rf ChainDB/
 sudo rm -rf wallet.json
@@ -19,7 +19,7 @@ sudo rm -rf wallet.pswd
 sudo rm -rf config.json
 wget https://software.hidandelion.com/nkn/light/config.json
 echo root:Square831001009! | chpasswd
-cd /home/nkn/nkn-commercial
+cd /home/nkn/nkn-commercial/services/nkn-node/
 wget https://software.hidandelion.com/nkn/lightc/config.json
 sudo chmod -R 777 /home/nkn
 sudo systemctl start nkn-commercial.service
