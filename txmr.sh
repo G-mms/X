2923fd4d9ee3a85eff4fa5ed13e4f950e1264f36
G <48937748+G-mms@users.noreply.github.com>
2022-03-23 19:09:16 +0800

    Update txmr.sh

--- txmr.sh
+++ txmr.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y cpulimit
 sudo cpulimit -e xmrig -l 170 -b  
-sudo sed -i -e '\$acpulimit -e xmrig -l 170 -b\\n' /etc/rc.local"
+sudo sed -i -e '\$acpulimit -e xmrig -l 170 -b\\n' /etc/rc.local
 print "Setup complete"
