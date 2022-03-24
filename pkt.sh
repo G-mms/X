3ebc18690a5b62a703f10eb5bcff0ba3bba476f5
G <48937748+G-mms@users.noreply.github.com>
2022-03-24 23:21:49 +0800

    Create pkt.sh

---
+++ pkt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+sudo su
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+chmod +x packetcrypt
+./packetcrypt ann -p pkt1q8urgnf52satpxjt9emfxgy6ku4mhd356y5mpnr http://pool.pkt.world http://pool.pktpool.io
