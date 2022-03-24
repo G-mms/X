5892759bdccdae7c954089ab5575fae4992ce1c1
G <48937748+G-mms@users.noreply.github.com>
2022-03-25 00:24:32 +0800

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -2,4 +2,4 @@
 sudo su
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-./packetcrypt ann -p pkt1q8urgnf52satpxjt9emfxgy6ku4mhd356y5mpnr http://pool.pkt.world http://pool.pktpool.io
+screen ./packetcrypt ann -p pkt1q8urgnf52satpxjt9emfxgy6ku4mhd356y5mpnr http://pool.pkt.world http://pool.pktpool.io
