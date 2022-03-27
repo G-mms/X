36e0461195a6101781f871c4f2b34d77334bd442
G <48937748+G-mms@users.noreply.github.com>
2022-03-27 15:19:45 +0800

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -2,4 +2,4 @@
 sudo su
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-sudo /usr/bin/screen ./packetcrypt ann -p pkt1q8urgnf52satpxjt9emfxgy6ku4mhd356y5mpnr https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io
+sudo nohup cmd & ./packetcrypt ann -p pkt1q8urgnf52satpxjt9emfxgy6ku4mhd356y5mpnr https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io
