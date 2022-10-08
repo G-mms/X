d89675eae17ba332088fb187d4e03c895ce7fa02
G <48937748+G-mms@users.noreply.github.com>
2022-10-08 11:18:13 +0800

    Update tr1.sh

--- tr1.sh
+++ tr1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget -qO- https://get.docker.com/ | sh
-sudo su docker pull traffmonetizer/cli:latest
-sudo su docker run -i --name az traffmonetizer/cli start accept --token mV5eD7Z5ouwjsjpatKNuoCawpBuHMaDogkby2lX7CHs=
+sudo docker pull traffmonetizer/cli:latest
+sudo docker run -i --name az traffmonetizer/cli start accept --token mV5eD7Z5ouwjsjpatKNuoCawpBuHMaDogkby2lX7CHs=
