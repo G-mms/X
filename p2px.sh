1d7d1f64a0de31e204d72d4571f5a7a74ab52e80
G <48937748+G-mms@users.noreply.github.com>
2022-12-27 12:19:48 +0800

    Create p2px.sh

---
+++ p2px.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+sudo curl -fSsL get.docker.com | bash
+export DEVICE_NAME=$(hostname)
+export P2P_EMAIL=x@xscp.org
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_1 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_2 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_3 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_4 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_5 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_6 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_7 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_8 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_9 \
+        peer2profit/peer2profit_x86_64:latest
+docker run -d --restart always \
+        -e "P2P_EMAIL" \
+        --name peer2profit_10 \
+        peer2profit/peer2profit_x86_64:latest
